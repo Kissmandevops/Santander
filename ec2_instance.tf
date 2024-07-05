2553a14d2f6161fee48ad8e6e7b6f5f66aadb3c7
Kissmandevops <kk37535@gmail.com>
2024-07-05 11:37:32 +0100

    Update ec2_instance.tf
    changed from unbutu to redhat

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "redhat" {
  most_recent = true
 
  filter {
    name   = "name"
-   values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+   values = ["redhat/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
  }
 
  filter {
@@ -21,10 +21,10 @@ data "aws_ami" "ubuntu" {
 locals {
  instances = {
    instance1 = {
-     ami           = data.aws_ami.ubuntu.id
+     ami           = data.aws_ami.redhat.id
      instance_type = "t2.micro"
    }
    instance2 = {
-     ami           = data.aws_ami.ubuntu.id
+     ami           = data.aws_ami.redhat.id
      instance_type = "t2.micro"
    }
